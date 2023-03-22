642bf8d53f03dccf25af793df29a1510180c2ea5
diegoG82 <diegogiardina@hotmail.it>
2023-03-22 15:41:58 +0100

    Pulizia js

--- js/script.js
+++ js/script.js
@@ -1,15 +1,5 @@
-// Utilizzando i dati forniti, creare un array di oggetti per rappresentare i membri del team.
-// Ogni membro è caratterizzato dalle seguenti informazioni: nome, ruolo e foto.
-// Wayne Barnett	Founder & CEO	        wayne-barnett-founder-ceo.jpg
-// Angela Caroll	Chief Editor	        angela-caroll-chief-editor.jpg
-// Walter Gordon	Office Manager	        walter-gordon-office-manager.jpg
-// Angela Lopez	Social Media Manager	angela-lopez-social-media-manager.jpg
-// Scott Estrada	Developer	            scott-estrada-developer.jpg
-// Barbara Ramos	Graphic Designer	    barbara-ramos-graphic-designer.jpg
-
 // MILESTONE 0:
 // Creare l’array di oggetti con le informazioni fornite.
-
 const ourTeam = [
   {
     name: "Wayne",
@@ -60,7 +50,6 @@ const ourTeam = [
 // const teamUl = document.querySelector("ul");
 // console.log(teamUl);
 
-
 // MILESTONE 2:
 // Stampare le stesse informazioni su DOM sottoforma di stringhe
 
@@ -72,7 +61,6 @@ const ourTeam = [
 //     teamUl.innerHTML += `<li>${key}: ${teamList[key]}</li>`;
 //   }
 // }
-
 
 // BONUS 1:
 // Trasformare la stringa foto in una immagine effettiva
@@ -103,7 +91,6 @@ for (let i = 0; i < ourTeam.length; i++) {
   </div>`;
 }
 console.log(cards);
-
 
 // Manipolo il DOM per inserire le cards
 createRow.innerHTML = cards;
