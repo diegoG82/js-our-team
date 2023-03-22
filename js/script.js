dd681bb1dbbec42c888ab9b8f0160f980c900285
diegoG82 <diegogiardina@hotmail.it>
2023-03-22 13:09:51 +0100

    Milestone 0 completed

---
+++ js/script.js
@@ -0,0 +1,83 @@
+// Utilizzando i dati forniti, creare un array di oggetti per rappresentare i membri del team.
+// Ogni membro è caratterizzato dalle seguenti informazioni: nome, ruolo e foto.
+// Wayne Barnett	Founder & CEO	        wayne-barnett-founder-ceo.jpg
+// Angela Caroll	Chief Editor	        angela-caroll-chief-editor.jpg
+// Walter Gordon	Office Manager	        walter-gordon-office-manager.jpg
+// Angela Lopez	Social Media Manager	angela-lopez-social-media-manager.jpg
+// Scott Estrada	Developer	            scott-estrada-developer.jpg
+// Barbara Ramos	Graphic Designer	    barbara-ramos-graphic-designer.jpg
+
+
+// MILESTONE 1:
+// Stampare su console le informazioni di nome, ruolo e la stringa della foto
+// MILESTONE 2:
+// Stampare le stesse informazioni su DOM sottoforma di stringhe
+// BONUS 1:
+// Trasformare la stringa foto in una immagine effettiva
+// BONUS 2:
+// Organizzare i singoli membri in card/schede. Se non vi sentite particolarmente creativi, potete prendere uno spunto dallo screenshot allegato.
+
+
+
+// MILESTONE 0:
+// Creare l’array di oggetti con le informazioni fornite.
+
+
+
+const ourTeam = [{
+
+    name : "Wayne",
+    surname: "Barnett",
+    role: "Founder & CEO",
+    imagePersonal : "wayne-barnett-founder-ceo.jpg",
+
+},
+
+{
+
+    name : "Angela",
+    surname: "Caroll",
+    role: "Chief Editor",
+    imagePersonal : "angela-caroll-chief-editor.jpg",
+
+},
+
+{
+
+    name : "Walter",
+    surname: "Gordon",
+    role: "Office Manager",
+    imagePersonal : "walter-gordon-office-manager.jpg"
+
+},
+
+{
+
+    name : "Angela ",
+    surname: "Lopez",
+    role: "Social Media manager",
+    imagePersonal : "angela-lopez-social-media-manager.jpg",
+
+},
+
+{
+
+    name : "Scott",
+    surname: "Estrada",
+    role: "Developer",
+    imagePersonal : "scott-estrada-developer.jpg",
+
+},
+
+{
+
+    name : "Barbara ",
+    surname: "Ramos",
+    role: "Graphic Designer	",
+    imagePersonal : "barbara-ramos-graphic-designer.jpg",
+
+},
+
+];
+
+
